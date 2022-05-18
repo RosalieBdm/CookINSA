599344fe04402baa8ddab1acca3bede458a4442d
josephbl74 <79768976+josephbl74@users.noreply.github.com>
2022-05-18 18:41:28 +0200

    showing images improvements

---
+++ store/index.js
@@ -0,0 +1,7 @@
+// the goal is accessing this from any component
+
+const state = () => ({
+    data1: "I'm centralized data1",
+    data2: "I'm centralized data2",
+    data3: "I'm centralized data3"
+})
